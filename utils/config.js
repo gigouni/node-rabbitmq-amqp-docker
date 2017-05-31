4caafeea0a087a974187986bc87387de8903b6fd
gigouni <nicolas.gigou@gmail.com>
2017-05-31 18:43:41 +0200

    Use the scotch-io/node-token-authentication repo to refound the project and have a better structure

---
+++ utils/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    'host': 'http://localhost',
+    'port': 8080,
+    'secret': 'toto',
+    'database': 'mongodb://localhost:27017/database_name',
+    'user': {
+        'name': 'root',
+        'password': 'root',
+        'is_admin': true,
+    }
+
+};
--- utils/constants.js
+++
@@ -1,9 +0,0 @@
-const _HOST             = 'localhost';
-const _PORT             = 3000;
-const _SECRET_JWT_KEY   = 'shared-secret-to-change';
-
-module.exports = {
-    HOST:               _HOST,
-    PORT:               _PORT,
-    SECRET_JWT_KEY:     _SECRET_JWT_KEY
-};
