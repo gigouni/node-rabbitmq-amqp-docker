d8475f9eecd3e53d09b9401afc04871f0e75a382
gigouni <nicolas.gigou@gmail.com>
2017-06-09 15:08:49 +0200

    Remove now-useless files

--- jwt_bundles/send_jwt/models/user.js
+++
@@ -1,9 +0,0 @@
-const MONGOOSE = require('mongoose');
-let Schema = MONGOOSE.Schema;
-
-// set up a MONGOOSE model
-module.exports = MONGOOSE.model('User', new Schema({
-	name: String, 
-	password: String, 
-	admin: Boolean 
-}));
